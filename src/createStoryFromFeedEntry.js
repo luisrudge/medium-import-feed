8f3135f523f7d4f58e8f86eebccd066b8a74d431
Lutz Roeder <lutzroeder@lutzroeder.com>
2015-12-27 18:47:08 -0500

    Support for publishedAt.

--- src/createStoryFromFeedEntry.js
+++ src/createStoryFromFeedEntry.js
@@ -1,11 +1,11 @@
 import moment from 'moment';
 
-export default ({title, description, categories, date}) => ({
+export default ({title, description, categories, pubdate}) => ({
   title,
   contentFormat: 'html',
   content: `<h1>${title}</h1>
-            <blockquote>This post was originally published on ${moment(date).format('YYYY-MM-DD')}</blockquote>
             ${description}`,
   tags: categories,
+  publishedAt: pubdate,
   publishStatus: 'draft',
 });
